077bef282a89d34b5bc5b8efe2b5fedb85517661
Kalie Ching <kaliefaith@gmail.com>
2026-06-11 23:43:10 -0400

    Add data gen function

--- linear_regression.py
+++ linear_regression.py
@@ -5,3 +5,16 @@
 # loss = mean((y_pred - y_true) ^ 2)
 # y_pred = X @ w + b
 # w = w - learning_rate * grad_w
+
+import jax.numpy as jnp
+import jax
+
+KEY = jax.random.PRNGKey(0)
+
+def generate_data(w=2.0, b=1.0, noise_scale=0.1):
+    X = jax.random.normal(KEY, (100, 1)) # returns 100x1 matrix of random values
+    noise = jax.random.normal(KEY, (100, 1))
+    data_to_regress = X * w + b + noise_scale * noise
+    return data_to_regress
+
+print(generate_data())
