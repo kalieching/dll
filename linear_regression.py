a7f8b444a5ba1383b31f301a1f4e67f0cab45dce
Kalie Ching <kaliefaith@gmail.com>
2026-06-11 20:51:29 -0400

    Initial commit for linear regression in jax

---
+++ linear_regression.py
@@ -0,0 +1,7 @@
+# Goal: Study Jax
+# Mini-project: Linear regression implemented from scratch with Jax
+
+# Linear regression:
+# loss = mean((y_pred - y_true) ^ 2)
+# y_pred = X @ w + b
+# w = w - learning_rate * grad_w
